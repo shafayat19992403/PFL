81df9aa980af6af9c0946ecd0cbc321d3ecf4a05
Md Shafayatul Haque <shafayat@Mds-MacBook-Air-2.local>
2024-02-27 04:56:14 +0600

    FMNIST

--- fl_client.py
+++ fl_client.py
@@ -13,6 +13,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
 from torchvision.datasets import CIFAR10
+from torchvision.datasets import MNIST
+from torchvision.datasets import FashionMNIST
 from torchvision.transforms import Compose, Normalize, ToTensor
 from tqdm import tqdm
 
